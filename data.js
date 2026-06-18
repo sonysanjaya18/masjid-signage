53689fedd057418b2daee02d4b0f7eb2f2eac914
sonysanjaya18 <133745985+sonysanjaya18@users.noreply.github.com>
2026-06-18 11:57:04 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,153 @@
+window.localMosqueData = {
+  "profile": {
+    "name": "Masjid Baiturrahman",
+    "tagline": "Mari merapat dan makmurkan masjid dengan Sholat Berjamaah Tepat Waktu."
+  },
+  "dashboard": {
+    "bgImage": "assets/masjid.jpg"
+  },
+  "kas": {
+    "tanggal": "18 Juni 2026",
+    "total": "Rp 24.500.000",
+    "umum": "Rp 14.000.000",
+    "pembangunan": "Rp 10.500.000",
+    "pengeluaran": "Rp 4.200.000"
+  },
+  "geo": {
+    "lat": -6.897096952815149,
+    "lng": 112.09740785397312,
+    "tz": 7
+  },
+  "correction": {
+    "subuh": null,
+    "dzuhur": 1,
+    "ashar": 47,
+    "maghrib": null,
+    "isya": null
+  },
+  "arusKas": [
+    {
+      "tgl": "16/06",
+      "ket": "Bayar Listrik & Air",
+      "kat": "Umum",
+      "masuk": "Rp 0",
+      "keluar": "500.000"
+    },
+    {
+      "tgl": "",
+      "ket": "Bayar Listrik & Air",
+      "kat": "Umum",
+      "masuk": "Rp 0",
+      "keluar": "Rp 500.0000"
+    },
+    {
+      "tgl": "",
+      "ket": "Bayar Listrik & Air",
+      "kat": "Umum",
+      "masuk": "Rp 0",
+      "keluar": "Rp 0500.000"
+    },
+    {
+      "tgl": "",
+      "ket": "Bayar Listrik & Air",
+      "kat": "Umum",
+      "masuk": "Rp 0",
+      "keluar": "Rp 0500.000"
+    },
+    {
+      "tgl": "",
+      "ket": "Bayar Listrik & Air",
+      "kat": "Umum",
+      "masuk": "Rp 0",
+      "keluar": "Rp 0500.000"
+    },
+    {
+      "tgl": "",
+      "ket": "Bayar Listrik & Air",
+      "kat": "Umum",
+      "masuk": "Rp 0",
+      "keluar": "Rp 0500.000"
+    },
+    {
+      "tgl": "",
+      "ket": "Bayar Listrik & Air",
+      "kat": "Umum",
+      "masuk": "Rp 0",
+      "keluar": "Rp 0"
+    }
+  ],
+  "donasi": [
+    {
+      "nama": "Hamba Allah",
+      "jml": "Rp 500.000",
+      "via": "Transfer Mandiri"
+    },
+    {
+      "nama": "Hamba Allah",
+      "jml": "TV Monitor",
+      "via": "Tunai"
+    },
+    {
+      "nama": "Hamba Allah",
+      "jml": "Rp 500.000",
+      "via": "Tunai"
+    },
+    {
+      "nama": "Hamba Allah",
+      "jml": "Rp 500.000",
+      "via": "Tunai"
+    },
+    {
+      "nama": "Hamba Allah",
+      "jml": "Rp 500.000",
+      "via": "Tunai"
+    },
+    {
+      "nama": "Hamba Allah",
+      "jml": "Rp 500.000",
+      "via": "Tunai"
+    },
+    {
+      "nama": "Hamba Allah",
+      "jml": "Rp 500.000",
+      "via": "Tunai"
+    },
+    {
+      "nama": "Hamba Allah",
+      "jml": "Rp 500.000",
+      "via": "Tunai"
+    },
+    {
+      "nama": "Hamba Allah",
+      "jml": "Rp 500.000",
+      "via": "Tunai"
+    }
+  ],
+  "events": [
+    {
+      "judul": "Kajian Umum Spesial",
+      "ustadz": "Ustadz Wijayanto",
+      "tgl": "Sabtu, 20 Juni 2026",
+      "jam": "19.30 WIB - Selesai",
+      "tempat": "Masjid Baiturrahman (Ruang Utama)",
+      "poster": "assets/poster1.jpg",
+      "desc": "Terbuka untuk jamaah umum muslimin dan muslimat."
+    },
+    {
+      "judul": "adasefwefwefd",
+      "ustadz": "",
+      "tgl": "",
+      "jam": "",
+      "tempat": "sadasdasdas",
+      "poster": "assets/poster1.jpg",
+      "desc": "asdasdas"
+    }
+  ],
+  "dokumentasi": {
+    "show": false,
+    "judul": "Dokumentasi Kegiatan Sosial Masjid",
+    "images": [
+      "assets/kegiatan1.jpg"
+    ]
+  }
+};
