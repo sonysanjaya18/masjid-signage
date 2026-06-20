5b6245bcc3692fa38e3cff2746daa14e3b0bf7c0
sonysanjaya18 <133745985+sonysanjaya18@users.noreply.github.com>
2026-06-20 11:27:14 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,62 +1,252 @@
-window.localMosqueData = {
+var localMosqueData = {
   "profile": {
-    "name": "Masjid Baiturrahman",
-    "tagline": "Mari merapat dan makmurkan masjid dengan Sholat Berjamaah Tepat Waktu."
+    "name": "Masjid Baiturrahman Panjunan",
+    "tagline": "Selamat datang di Masjid Baiturrahman Pajunan, desa tasikmadu kecamatan palang kabupaten tuban | Harap Matikan/Silent Telepon Seluler Untuk Kekhusyukan Ibadah",
+    "subHeadline": "Dusun Panjunan Desa Tasikmadu Kec. Palang - Tuban"
   },
   "dashboard": {
     "bgImage": "foto_masjid1.jpeg"
   },
+  "struktur": {
+    "penasehat": [
+      {
+        "nama": "Ust. Kambali Jab",
+        "jabatan": "Penasehat I",
+        "foto": ""
+      },
+      {
+        "nama": "Bpk. Salahudin",
+        "jabatan": "Penasehat II",
+        "foto": ""
+      }
+    ],
+    "ketua": "H. Moh. Soleh",
+    "ketuaFoto": "",
+    "wakilKetua": "Ust. Mohammad Heri",
+    "wakilKetuaFoto": "",
+    "sekretaris1": "Sdr. Sony Sanjaya",
+    "sekretaris1Foto": "",
+    "sekretaris2": "Sdr. Hendro",
+    "sekretaris2Foto": "",
+    "bendahara1": "Bpk. Abd. Rozaq",
+    "bendahara1Foto": "",
+    "bendahara2": "Bpk. Nadhifan",
+    "bendahara2Foto": "",
+    "seksi": [
+      {
+        "nama": "Dakwah & Peribadatan",
+        "anggota": [
+          {
+            "nama": "Ust. Abdurrohman",
+            "foto": ""
+          },
+          {
+            "nama": "Ust. Ali Usman",
+            "foto": ""
+          },
+          {
+            "nama": "Ust. Wahyudi",
+            "foto": ""
+          }
+        ]
+      },
+      {
+        "nama": "Pembangunan",
+        "anggota": [
+          {
+            "nama": "Bpk. Zainuri",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. M Adib",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Umar",
+            "foto": ""
+          }
+        ]
+      },
+      {
+        "nama": "Humas & Kemasyarakatan",
+        "anggota": [
+          {
+            "nama": "Bpk. Budi",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Saiful Anam",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. A. Syarifudin",
+            "foto": ""
+          }
+        ]
+      },
+      {
+        "nama": "Kepemudaan",
+        "anggota": [
+          {
+            "nama": "Bpk. Ahmad Khoirul Hakim",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Malik",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Sugeng Priyanto",
+            "foto": ""
+          },
+          {
+            "nama": "Sdr. Ragil",
+            "foto": ""
+          }
+        ]
+      },
+      {
+        "nama": "Sarana & Prasarana",
+        "anggota": [
+          {
+            "nama": "Bpk. Rouf",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. M. Nadhif",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Mashud",
+            "foto": ""
+          }
+        ]
+      },
+      {
+        "nama": "Kebersihan",
+        "anggota": [
+          {
+            "nama": "Bpk. Kasdu",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Kusnan",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Juwaini",
+            "foto": ""
+          }
+        ]
+      },
+      {
+        "nama": "Keamanan",
+        "anggota": [
+          {
+            "nama": "Bpk. Hadi",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Yanto",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Khusnul Yaqin",
+            "foto": ""
+          }
+        ]
+      },
+      {
+        "nama": "PHBI",
+        "anggota": [
+          {
+            "nama": "Ust. Musa",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. H. Abu Khamid",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Romli",
+            "foto": ""
+          },
+          {
+            "nama": "Bpk. Darwoto",
+            "foto": ""
+          }
+        ]
+      }
+    ],
+    "show": true
+  },
   "kas": {
-    "tanggal": "18 Juni 2026",
-    "total": "Rp 24.500.000",
-    "umum": "Rp 14.000.000",
-    "pembangunan": "Rp 10.500.000",
-    "pengeluaran": "Rp 4.200.000"
-  },
-  "geo": {
-    "lat": -6.897096952815149,
-    "lng": 112.09740785397312,
-    "tz": 7
-  },
-  "correction": {
-    "subuh": null,
-    "dzuhur": 1,
-    "ashar": 47,
-    "maghrib": null,
-    "isya": null
+    "tanggal": "19 Juni 2026",
+    "umum": "Rp 13.675.000",
+    "pembangunan": "Rp 16.136.500",
+    "pengeluaran": "-"
   },
   "arusKas": [
     {
-      "tgl": "16/06",
-      "ket": "Bayar Listrik & Air",
+      "tgl": "19/06",
+      "ket": "Infak Jum'at",
       "kat": "Umum",
-      "masuk": "Rp 0",
-      "keluar": "500.000"
+      "masuk": "Rp 768.000",
+      "keluar": "Rp 0"
     }
   ],
   "donasi": [
     {
-      "nama": "Hamba Allah",
-      "jml": "Rp 500.000",
-      "via": "Transfer Mandiri"
+      "nama": "Bpk. M Adib - TV Monitor",
+      "jml": "1 Unit",
+      "via": "Barang"
     }
   ],
+  "khatib": {
+    "show": true,
+    "featuredImage": "jumat.png",
+    "list": [
+      {
+        "tgl": "26 Juni 2026",
+        "nama": "Ust. Abdurrohman"
+      },
+      {
+        "tgl": "3 Juli 2026",
+        "nama": "Ust. Wahyudi"
+      },
+      {
+        "tgl": "3 Juli 2026",
+        "nama": "Ust. Kambali Jab"
+      }
+    ]
+  },
   "events": [
     {
-      "judul": "Kajian Umum Spesial",
+      "judul": "Maulid Nabi Muhammad SAW",
       "ustadz": "Ustadz Wijayanto",
-      "tgl": "Sabtu, 20 Juni 2026",
-      "jam": "19.30 WIB - Selesai",
-      "tempat": "Masjid Baiturrahman (Ruang Utama)",
+      "tgl": "Senin, 24 Agustus 2026",
+      "jam": "19.30 WIB (Ba'dha Isya')",
+      "tempat": "Masjid Baiturrahman",
       "poster": "poster_kajian1.jpeg",
-      "desc": "Terbuka untuk jamaah umum muslimin dan muslimat."
+      "desc": "Dimohon untuk semua jama'ah membawa 5 kardus berkat atau snack"
     }
   ],
   "dokumentasi": {
-    "show": true,
+    "show": false,
     "judul": "Dokumentasi Kegiatan Sosial Masjid",
     "images": [
       "assets/kegiatan1.jpg"
     ]
+  },
+  "geo": {
+    "kotaId": "1628",
+    "kotaNama": "KAB. TUBAN"
+  },
+  "correction": {
+    "subuh": 0,
+    "dzuhur": 0,
+    "ashar": 0,
+    "maghrib": 0,
+    "isya": 0
   }
 };
