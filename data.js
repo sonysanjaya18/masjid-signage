fe0acc432b852d4cba55e8944d044b47571ae431
sonysanjaya18 <133745985+sonysanjaya18@users.noreply.github.com>
2026-06-20 12:25:15 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -217,6 +217,10 @@ var localMosqueData = {
       {
         "tgl": "3 Juli 2026",
         "nama": "Ust. Kambali Jab"
+      },
+      {
+        "tgl": "10 Juli 2026",
+        "nama": "Ust. Ali Usman"
       }
     ]
   },
